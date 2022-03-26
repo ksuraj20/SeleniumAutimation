6e6da730c03085804ae94e7fb4324ddd914a0970
Suraj K <89332188+ksuraj20@users.noreply.github.com>
2022-03-26 22:31:30 +0530

    Create CalculateAge.java

---
+++ CalculateAge.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package Interview;
+
+import java.util.Scanner;
+
+/**
+ * @author suraj
+ *
+ */
+public class Demo
+	{
+
+		/**
+		 * @param args
+		 */
+		public static void main(String[] args)
+			{
+				//Get Input data from console
+				Scanner sc = new Scanner(System.in);
+				System.out.println("Enter your name");
+				String nameString= sc.nextLine();
+				System.out.println("Enter your DOB");
+				String DOB = sc.nextLine();
+				
+				//operation
+				//calculate the age as of today
+				
+				String[] parts = DOB.split("\\-");
+				String Date= parts[0];
+				String Month= parts[1];
+				String Year= parts[2];
+				
+				int birthDay,birthMonth,birthYear;
+				birthDay=Integer.parseInt(Date);
+				birthMonth=Integer.parseInt(Month);
+				birthYear=Integer.parseInt(Year);
+				
+				int currentDate=26;
+				int currentMonth=03;
+				int currentYear=2022;
+				
+				if(birthDay>currentDate)
+					{
+						currentDate= currentDate + currentMonth-1;
+						currentMonth=currentMonth-1;
+					}
+				if(birthMonth>currentMonth)
+					{
+						currentYear = currentYear-1;
+						currentMonth=currentMonth+12;
+					}
+				
+				int Days= currentDate-birthDay;
+				int Months= currentMonth-birthMonth;
+				int Years= currentYear-birthYear;
+				System.out.println();
+				System.out.println(nameString+", your age is "+Years+" Years "+Months+" Months "+Days+" Days");
+				
+			}
+
+	}
