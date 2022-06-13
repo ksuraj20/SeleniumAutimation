5d60bd6e5e035940e06a0e4dc9209559dd919ba5
Suraj Kathare <89332188+ksuraj20@users.noreply.github.com>
2022-06-13 14:32:28 +0530

    Create ReverseArrayString.java

---
+++ ReverseArrayString.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package newPackage;
+
+/**
+ * @author suraj
+ *
+ */
+public class ReverseArrayString
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+		{
+
+			String[] input = {"abc","mno","xyz"};
+			String[] reverseInput = new String[input.length];
+			String[] outputList = new String[input.length]; 
+			int j=0;
+			String output = "",temp="";
+			char c;
+
+			//Print the inpur array
+			System.out.print("Input:- ");
+			for(String in : input)
+				System.out.print(in+", ");
+			
+			System.out.println("");
+			
+			//Reverse array
+			for(int i=input.length-1;i >=0;i--)
+				{
+					reverseInput[j]=input[i];
+					j++;
+				}
+
+
+			//Reverse the array element
+			for (int k=0; k< reverseInput.length; k++)
+				{
+					temp=reverseInput[k];
+					for(int l=reverseInput.length-1; l >=0; l--)
+						{
+							c=temp.charAt(l);
+							output+=c;
+						}
+					outputList[k]=output;
+					output="";
+				}
+
+			//Print the output array
+			System.out.print("Output:- ");
+			for(String dc : outputList)
+				{
+					System.out.print(dc+", ");
+				}
+		}
+
+}
